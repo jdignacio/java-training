bda2f0f55bff386add9ea6eb4c6d2ca058542530
jdignacio <59072324+jdignacio@users.noreply.github.com>
2019-12-20 09:55:35 -0500

    Update HelloWorld.java

--- src/Basics/HelloWorld.java
+++ src/Basics/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Wolrd!!");
 		System.out.println("I'm adding codes to my project.");
+		System.out.println("Adding code from GitHub.");
 	}
 	
 
